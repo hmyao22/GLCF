0571ff1b94ec5d1bf15d7a4b4439c4fdc452ab5f
hmyao22 <136300324+hmyao22@users.noreply.github.com>
2023-08-11 09:52:15 +0800

    Update bottleneck.py

--- models/bottleneck.py
+++ models/bottleneck.py
@@ -16,9 +16,9 @@ from models.utils import get_2d_sincos_pos_embed
 
 
 class DPTV(nn.Module):
-    def __init__(self, img_size=[28, 14, 7], patch_size=[4, 2, 1], in_chans=[192, 384, 768],
-                 embed_dim=240, output_dim=768, depth=6, num_heads=12,
-                 decoder_embed_dim=240, decoder_depth=6, decoder_num_heads=16,
+    def __init__(self, img_size=[28, 14, 7], patch_size=[4, 2, 1], in_chans=[512, 1024, 2048],
+                 embed_dim=480, output_dim=2048, depth=6, num_heads=12,
+                 decoder_embed_dim=480, decoder_depth=6, decoder_num_heads=16,
                  mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False):
         super().__init__()
         self.embed_dim = embed_dim
@@ -110,16 +110,17 @@ class DPTV(nn.Module):
             nn.init.constant_(m.weight, 1.0)
 
     def forward_encoder(self, x):
+        input_x = x
         ################################ multi-scale embedding ##################################
         x1 = self.patch_embed1(x[0])
         x2 = self.patch_embed2(x[1])
         x3 = self.patch_embed3(x[2])
-        x = x1+x2+x3
+        x = x3
         x = x + self.pos_embed[:, 1:, :]
         N, L, D = x.shape  # batch, length, dim
 
         ############################################ v1 #########################################
-
+        #
         # ########### sematic token cat #############
         # auxiliary_token = self.auxiliary_token.repeat(N, L, 1)
         # auxiliary_token = auxiliary_token + self.pos_embed[:, 1:, :]
@@ -127,7 +128,7 @@ class DPTV(nn.Module):
         #
         # ############### encoder ################
         # for encoder_phase, blk in enumerate(self.blocks):
-        #     x_ = blk(x_)
+        #     x_ = blk(x_)+x_
         # x_ = self.norm(x_)
         # x_global_latent = x_[:, L:, :]
         # x_local_latent = x_[:, :L, :]
@@ -137,8 +138,8 @@ class DPTV(nn.Module):
         # x_local_latent = self.decoder_embed(x_local_latent)
         # ########## decoder ###########
         # for gl_blk, lo_blk in zip(self.global_decoder_blocks, self.local_decoder_blocks):
-        #     x_global_latent = gl_blk(x_global_latent)
-        #     x_local_latent = lo_blk(x_local_latent)
+        #     x_global_latent = gl_blk(x_global_latent)+x_global_latent
+        #     x_local_latent = lo_blk(x_local_latent)+x_local_latent
         #
         # x_global_latent = self.norm(x_global_latent)
         # x_global_output = self.decoder_pred(x_global_latent)
@@ -148,51 +149,14 @@ class DPTV(nn.Module):
 
         ############################################ v2 #########################################
 
-        ########### sematic token cat #############
-        auxiliary_token = self.auxiliary_token.repeat(N, L, 1)
-        auxiliary_token = auxiliary_token + self.pos_embed[:, 1:, :]
-        x_ = torch.cat([x, auxiliary_token], dim=1)
-
-        ############### encoder ################
-        for encoder_phase, blk in enumerate(self.blocks):
-            x_ = blk(x_)
-        x_ = self.norm(x_)
-        x_global_latent = x_[:, L:, :]
-        x_local_latent = x_[:, :L, :]
-
-        ########## encoder-decoder ###########
-        x_global_latent = self.decoder_embed(x_global_latent)
-        x_local_latent = self.decoder_embed(x_local_latent)
-
-        # ######### latent-pos-embedding ###########
-        latent_auxiliary_token = self.latent_auxiliary_token.repeat(N, L, 1)
-        latent_auxiliary_token = latent_auxiliary_token + self.pos_embed[:, 1:, :]
-
-        ########### decoder global #############
-        x_global_latent = torch.cat([x_global_latent, latent_auxiliary_token], dim=1)
-        for decoder_phase, blk in enumerate(self.global_decoder_blocks):
-            x_global_latent = blk(x_global_latent)
-
-        x_global_latent = self.norm(x_global_latent)
-        x_global_output = self.decoder_pred(x_global_latent)
-        x_global_output = x_global_output[:, -L:, :]
-
-        ########### decoder local #############
-        for decoder_phase, blk in enumerate(self.local_decoder_blocks):
-            x_local_latent = blk(x_local_latent)
-
-        x_local_latent = self.norm(x_local_latent)
-        x_local_output = self.decoder_pred(x_local_latent)
-
-        ############################################ v3 #########################################
-
         # ########### sematic token cat #############
-        # auxiliary_token = self.auxiliary_token.repeat(N, 1, 1)
+        # auxiliary_token = self.auxiliary_token.repeat(N, L, 1)
+        # auxiliary_token = auxiliary_token + self.pos_embed[:, 1:, :]
         # x_ = torch.cat([x, auxiliary_token], dim=1)
         #
         # ############### encoder ################
         # for encoder_phase, blk in enumerate(self.blocks):
-        #     x_ = blk(x_)
+        #     x_ = blk(x_)+x_
         # x_ = self.norm(x_)
         # x_global_latent = x_[:, L:, :]
         # x_local_latent = x_[:, :L, :]
@@ -208,7 +172,7 @@ class DPTV(nn.Module):
         # ########### decoder global #############
         # x_global_latent = torch.cat([x_global_latent, latent_auxiliary_token], dim=1)
         # for decoder_phase, blk in enumerate(self.global_decoder_blocks):
-        #     x_global_latent = blk(x_global_latent)
+        #     x_global_latent = blk(x_global_latent)+x_global_latent
         #
         # x_global_latent = self.norm(x_global_latent)
         # x_global_output = self.decoder_pred(x_global_latent)
@@ -216,31 +180,87 @@ class DPTV(nn.Module):
         #
         # ########### decoder local #############
         # for decoder_phase, blk in enumerate(self.local_decoder_blocks):
-        #     x_local_latent = blk(x_local_latent)
+        #     x_local_latent = blk(x_local_latent)+x_local_latent
         #
         # x_local_latent = self.norm(x_local_latent)
         # x_local_output = self.decoder_pred(x_local_latent)
 
+        ############################################ v3 #########################################
+
+        ########## sematic token cat #############
+        auxiliary_token = self.auxiliary_token.repeat(N, 1, 1)
+        x_ = torch.cat([x, auxiliary_token], dim=1)
+
+        ############### encoder ################
+        for encoder_phase, blk in enumerate(self.blocks):
+            x_ = blk(x_)+x_
+        x_ = self.norm(x_)
+        x_global_latent = x_[:, L:, :]
+        x_local_latent = x_[:, :L, :]
+
+        ########## encoder-decoder ###########
+        x_global_latent = self.decoder_embed(x_global_latent)
+        x_local_latent = self.decoder_embed(x_local_latent)
+
+        # ######### latent-pos-embedding ###########
+        latent_auxiliary_token = self.latent_auxiliary_token.repeat(N, L, 1)
+        latent_auxiliary_token = latent_auxiliary_token + self.pos_embed[:, 1:, :]
+
+        ########### decoder global #############
+        x_global_latent = torch.cat([x_global_latent, latent_auxiliary_token], dim=1)
+        for decoder_phase, blk in enumerate(self.global_decoder_blocks):
+            x_global_latent = blk(x_global_latent)+x_global_latent
+
+        x_global_latent = self.decoder_norm(x_global_latent)
+        x_global_output = self.decoder_pred(x_global_latent)
+        x_global_output = x_global_output[:, -L:, :]
+
+        ########### decoder local #############
+        for decoder_phase, blk in enumerate(self.local_decoder_blocks):
+            x_local_latent = blk(x_local_latent)+x_local_latent
+
+        x_local_latent = self.decoder_norm(x_local_latent)
+        x_local_output = self.decoder_pred(x_local_latent)
+
+        return self.unpatchify(x_local_output).contiguous(), self.unpatchify(x_global_output).contiguous()
+
+
+        ##########################################w/o sb #####################
+        # return input_x[2], input_x[2]
 
 
 
-        return x_local_output, x_global_output
 
     def forward(self, imgs):
         pred = self.forward_encoder(imgs)
 
         return pred
 
+    def unpatchify(self, x):
+        """
+        x: (N, L, patch_size**2 *3)
+        imgs: (N, 3, H, W)
+        """
+        p = 1
+        h = w = int(x.shape[1] ** .5)
+        c = int(x.shape[-1])
+        assert h * w == x.shape[1]
+
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, c))
+        x = torch.einsum('nhwpqc->nchpwq', x)
+        imgs = x.reshape(shape=(x.shape[0], c, h * p, h * p))
+        return imgs
+
 
 if __name__ == '__main__':
     import time
     import torch
 
-    model = DPTV()
-    input_tensor = [torch.rand(1, 96, 56, 56), torch.rand(1, 192, 28, 28),
-                    torch.rand(1, 384, 14, 14)]
+    model = DPTV().cuda()
+    input_tensor = [torch.rand(1, 512, 28, 28).cuda(), torch.rand(1, 1024, 14, 14).cuda(), torch.rand(1, 2048, 7, 7).cuda()]
     for i in range(10):
         t1 = time.time()
         output = model.forward(input_tensor)
         t2 = time.time()
         print(t2 - t1)
+        print(output[0].shape)
